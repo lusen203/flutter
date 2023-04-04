a74b5e75c8a34be71c4d3efad3c2280c41d37cb5
lu sen <senlu03@gmail.com>
2023-04-04 19:13:15 +0700

    del

--- categories/android/app/src/main/kotlin/com/example/categories/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.categories
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- categories/lib/list_meals.dart
+++
@@ -1,5 +0,0 @@
-import 'dummy_data.dart';
-class ListMeals{
-  final List<Meal> list;
-  ListMeals(this.list);
-}
